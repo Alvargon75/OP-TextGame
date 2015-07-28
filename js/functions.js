812f48b5967dfbbf924e5af1eb07ad156896a065
Alvargon75 <alvarogonzalezlopez@hotmail.com>
2015-07-28 22:30:53 +0200

    Starting JS

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,14 @@
-﻿
+﻿/*
+Cosas que copiar comunes:
+
+	document.getElementById("").innerHTML = "";
+	document.getElementById("").classList.add('');
+	document.getElementById("").classList.remove('');
+
+*/
+var start = function(){
+	console.log("Game Started");
+	document.getElementById("").innerHTML = "";
+	document.getElementById("").classList.add('');
+	document.getElementById("").classList.remove('');
+}
