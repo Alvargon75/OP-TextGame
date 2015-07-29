079c3b6514fe5925e0450bacf56a15272630e427
Alvargon75 <alvarogonzalezlopez@hotmail.com>
2015-07-29 15:02:37 +0200

    Fixes

--- js/functions.js
+++ js/functions.js
@@ -16,9 +16,6 @@ var start = function(){
 	document.getElementById("d1_b").classList.remove('hidden');
 }
 var marine = function(){
-	return true;
-	pirata = false;
-	console.log("Marine =" + " " + marine)
 	document.getElementById("pirata_marine").innerHTML = "Decides ir con el barco de los marines. Allí encuentras al Teniente Fullbody que te recibe amablemente. Te invita a ir con el hasta la Villa de las Conchas o unirte a la Marina. <br /> ¿Qué eliges?";
 	document.getElementById("d1_a").classList.add('hidden');
 	document.getElementById("d1_b").classList.add('hidden');
@@ -26,8 +23,9 @@ var marine = function(){
 	document.getElementById("d2m_b").classList.remove('hidden');
 }	
 var pirata = function(){
-	return true;
-	marine = false;
-	console.log("Pirata =" + " " + pirata)
 	document.getElementById("pirata_marine").innerHTML = "No sabes por qué pero no quieres ir con los marines, prefieres ir con los piratas. Remas el barril hasta el barco pirata y escalas hasta arriba. <br /> Allí ves a la capitana que te quiere ejecutar. <br /> ¿Vas a morir o a luchar?";
+	document.getElementById("d1_a").classList.add('hidden');
+	document.getElementById("d1_b").classList.add('hidden');
+	document.getElementById("d2p_a").classList.remove('hidden');
+	document.getElementById("d2p_b").classList.remove('hidden');
 }
