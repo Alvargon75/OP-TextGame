e69a6e0d19c055be557bc831200bfc0bc7ee4f3e
Alvargon75 <alvarogonzalezlopez@hotmail.com>
2015-07-29 11:26:18 +0200

    <p></p>

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,14 @@ Cosas que copiar comunes:
 */
 var start = function(){
 	console.log("Game Started");
-	document.getElementById("").innerHTML = "";
-	document.getElementById("").classList.add('');
-	document.getElementById("").classList.remove('');
+	document.getElementById("intro").innerHTML = "Te encuentras en un barril en medio del mar, a lo lejos ves dos barcos, uno con una enseña pirata y otro de la marina. <br/> ¿A cuál decides pedir ayuda?";
+	document.getElementById("inicio").classList.add('hidden');
+	document.getElementById("d1_a").classList.remove('hidden');
+	document.getElementById("d1_b").classList.remove('hidden');
+}
+var marine = function(){
+
+}
+var pirata = function(){
+
 }
