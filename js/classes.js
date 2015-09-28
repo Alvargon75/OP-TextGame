6edc25dbf5f57a4d559a98cd363c768d15aa3d4b
Alvargon75 <alvarogonzalezlopez@hotmail.com>
2015-09-28 16:30:05 +0200

    Preparando las clases

---
+++ js/classes.js
@@ -0,0 +1,12 @@
+var Character;
+
+Character = (function() {
+  function Character(name) {
+    this.name = name;
+  }
+
+  Character.prototype.attack = function() {};
+
+  return Character;
+
+})();
