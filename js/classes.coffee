6130aec54ea264d08915c1ad5c3854f17cd6c524
Alvargon75 <alvarogonzalezlopez@hotmail.com>
2015-10-01 22:34:58 +0200

    Cosillas sueltas

--- js/classes.coffee
+++ js/classes.coffee
@@ -14,7 +14,7 @@ class Pelea
 
         # Genera el escenario
         document.getElementById("combatUI").classList.remove('combatUI-inactive');
-        escribir("vs_title", personajeActual.name + " " + "vs" + " " + enemy.name);
+        escribir("vs_title", personajeActual.name + " " + "vs" + " " + @enemy.name);
         escribir("at1", personajeActual.ataques[0]);
         escribir("at2", personajeActual.ataques[1]);
         escribir("at3", personajeActual.ataques[2]);
@@ -24,6 +24,8 @@ class Pelea
 
         document.querySelectorAll('div#combatUI > visualUI > personaje > i#sprite > img')[0].src = personajeActual.data[0].sprite
         document.querySelectorAll('div#combatUI > visualUI > enemigo > i#sprite > img')[0].src = enemy.data[0].sprite;
+
+        document.querySelectorAll('div#combatUI visualUI personajes div.UI-pokemonbox table td').classList.add('green');
 
     generate: () ->
 
--- js/classes.js
+++ js/classes.js
@@ -18,7 +18,7 @@ Pelea = (function() {
       turnos: 0
     };
     document.getElementById("combatUI").classList.remove('combatUI-inactive');
-    escribir("vs_title", personajeActual.name + " " + "vs" + " " + enemy.name);
+    escribir("vs_title", personajeActual.name + " " + "vs" + " " + this.enemy.name);
     escribir("at1", personajeActual.ataques[0]);
     escribir("at2", personajeActual.ataques[1]);
     escribir("at3", personajeActual.ataques[2]);
@@ -27,6 +27,7 @@ Pelea = (function() {
     combatHUD();
     document.querySelectorAll('div#combatUI > visualUI > personaje > i#sprite > img')[0].src = personajeActual.data[0].sprite;
     document.querySelectorAll('div#combatUI > visualUI > enemigo > i#sprite > img')[0].src = enemy.data[0].sprite;
+    document.querySelectorAll('div#combatUI visualUI personajes div.UI-pokemonbox table td').classList.add('green');
   }
 
   Pelea.prototype.generate = function() {};
