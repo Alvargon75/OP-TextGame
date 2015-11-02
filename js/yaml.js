56fc0773e7b1a4c0127255a738b728a5b818681c
Alvargon75 <alvarogonzalezlopez@hotmail.com>
2015-11-02 19:38:25 +0100

    Anonymous Function en el AJAX de config.yaml

--- js/yaml.js
+++ js/yaml.js
@@ -21,20 +21,23 @@ var yamlRequest = function(url, callback){
 	ymlHttp.send();
 };
 
-var ymlHttp;
+(function(){
+	var ymlHttp;
 
-if(window.XMLHttpRequest){ // Not IE
-	ymlHttp = new XMLHttpRequest();
-}else{ // IE 7
-	ymlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-ymlHttp.open("GET", "config.yaml", true);
-
-ymlHttp.onreadystatechange = function(){
-	if(ymlHttp.readyState == 4 && ymlHttp.status == 200){
-		config = yaml.load(ymlHttp.responseText);
+	if(window.XMLHttpRequest){ // Not IE
+		ymlHttp = new XMLHttpRequest();
+	}else{ // IE 7
+		ymlHttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-};
 
-ymlHttp.send();
+	ymlHttp.open("GET", "config.yaml", true);
+
+	ymlHttp.onreadystatechange = function(){
+		if(ymlHttp.readyState == 4 && ymlHttp.status == 200){
+			window.config = yaml.load(ymlHttp.responseText);
+			console.warn("Error is supposed to happen.");
+		}
+	};
+
+	ymlHttp.send();
+})();
